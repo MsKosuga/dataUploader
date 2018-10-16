e9c061548d1d18ddf37ebbd33955913a184e7b83
MsKosuga <lutecia1600@gmail.com>
2018-10-17 02:02:05 +0900

    Implement upload proc

--- uploader.js
+++ uploader.js
@@ -1,18 +1,59 @@
 'use strict'
-var request = require('request');
 var fs = require('fs');
+var util = require('./util');
+var request = require('request');
 
-var url="http://localhost:8080/v1/nanogw/kura";
+const chkfile = "updata.dat"
+
+var curDate = util.getCurrentDate();
+var path = "./data/" + curDate;
+
+var lastFile = "";
+
+var url="<SERVER URL>";
 var formData = {
-	"timeout": 180000
+    "timeout": 180000
 }
 
-formData.file = fs.createReadStream("./data/20181015/19700101000000.jpeg");
+var files = fs.readdirSync(path);
+console.log(files);
 
-request.post({url:url, formData:formData}, function(err, response, body) {
-    if(err){
-      console.log(err)
-    }else{
-      console.log(body)
-    };
+fs.readFile(chkfile, 'utf-8', function(err, data){
+	if(err){
+		console.log(err);
+	} else {
+		data = data.replace(/\r?\n/g,"");
+		var pos = files.indexOf(data);
+		if(pos != files.length - 1){
+			files.splice(0, pos + 1);
+			console.log(files);
+				
+			uploadProc();
+		}else{
+			console.log("no upload data");
+		};
+	};
 });
+
+function uploadProc(){
+	var filename = path + "/" +  files[0];
+	console.log(filename);
+
+	formData.file = fs.createReadStream(filename);
+
+	request.post({url:url, formData:formData}, function(err, response, body){
+    	if(err){
+      		console.log(err)
+    	}else{
+      		console.log(body)
+			fs.writeFileSync(chkfile, files[0]);	
+			files.shift();
+			if(files.length != 0){
+				uploadProc();
+			}else{
+				console.log("uploadProc end.");
+			};
+    	};
+	});
+};
+
